1783951f6e358927309abb2ba083fcb3c9c31a30
cinsomnia <124067657+cinsomnia@users.noreply.github.com>
2023-01-31 22:55:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def prediction(sepal_length, sepal_width, petal_length, petal_width):
 
 def main():
     st.title("Aplikasi Prediksi Bunga Iris Algoritma Support Vector Machine")
-    st.markdown('Muhammad Fahmi (2019230099) | Universitas Darma Persada | UAS Datamining Malam')
+    st.markdown('')
     st.write('\n')
     st.markdown('Silakan isi form berikut :')
     
